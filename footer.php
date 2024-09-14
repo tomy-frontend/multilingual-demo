6b1f0cef770582ec6f5960e7b00939aa17479330
tomy.dev <xxx.d1231@gmail.com>
2024-09-14 16:12:26 +0900

    add file

---
+++ footer.php
@@ -0,0 +1,10 @@
+</main>
+
+<footer>
+    <div class="footer">
+        <div class="inner">
+            <small>&copy; <?php echo date('Y'); ?> tomy_dev. All rights reserved.</small>
+        </div>
+    </div>
+    <?php wp_footer(); ?>
+</footer>
