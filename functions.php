ebf4dab79bbadff0c19d42d4befc92d53ce68521
tomy.dev <xxx.d1231@gmail.com>
2024-10-12 22:55:56 +0900

    script.js重複のため削除

--- functions.php
+++ functions.php
@@ -16,12 +16,11 @@ function my_script_init()
     error_log("my_script_init");
     wp_enqueue_style("my-reset-style", "https://cdn.jsdelivr.net/npm/destyle.css@1.0.15/destyle.css", array(), null, "all");
     wp_enqueue_style("my-style", get_template_directory_uri() . "/css/style.css", array(), filemtime(get_theme_file_path("/css/style.css")), "all");
-    wp_enqueue_script("my-script", get_template_directory_uri() . "/js/script.js", array("jquery"), filemtime(get_theme_file_path("/js/script.js")), true);
-
     wp_enqueue_script("my-script-module", get_template_directory_uri() . "/js/script.js", array(), filemtime(get_theme_file_path("/js/script.js")), true);
     wp_script_add_data("my-script-module", "type", "module");
 }
 add_action("wp_enqueue_scripts", "my_script_init");
+
 
 
 
